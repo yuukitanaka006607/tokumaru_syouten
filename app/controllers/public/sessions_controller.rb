92a5fa8aab5138356d253ffa6e44c882b4c2f8d4
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2024-01-30 01:26:07 +0000

    [Finish_public_customer_edit]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
+  ef after_sign_in_path_for(resource)
+    root_path
+  end
 
+  def after_sign_out_path_for(resouce)
+    root_path
+  end
   # GET /resource/sign_in
   # def new
   #   super
@@ -24,4 +30,14 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+private
+
+  def customer_state
+    customer = Customer.find_by(email: params[:customer][:email])
+    if customer
+      if customer.valid_password?(params[:customer][:password])&& customer.is_active==false
+        redirect_to "/customers/sign_up"
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ devise_for :admin, skip: [:registrations, :passwords], controllers: {
 root to: "public/homes#top"
 scope module: :public do
   get "customers/my_page" => "customers#show"
+  get "customers/information/edit" => "customers#edit"
+  patch "customers/information" => "customers#update"
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
