6b98c16a27a3d3bcbe50a2fa04fe82070b903921
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2024-02-23 15:26:31 +0000

    [public_layaut]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
   has_many :cart_items
+  has_many :orders
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
